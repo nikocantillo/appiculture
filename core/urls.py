cd338f89867622790209c4b05b8a1fdef0293d9f
nikocantillo <devoxomy@gmail.com>
2022-12-11 22:10:06 -0500

    creating another nav

--- core/urls.py
+++ core/urls.py
@@ -13,3 +13,7 @@ urlpatterns = [
     path('courses/', include(('courses.urls'), namespace='courses')),
 
 ]
+
+if(settings.DEBUG):
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
