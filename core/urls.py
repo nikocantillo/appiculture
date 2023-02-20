b51dc0d6bf4f03758739c8d040fd8b847c6ed1af
nikocantillo <devoxomy@gmail.com>
2023-02-19 21:49:32 -0500

    modify the urls

--- core/urls.py
+++ core/urls.py
@@ -13,8 +13,7 @@ urlpatterns = [
     path('courses/', include(('courses.urls'), namespace='courses')),
     path('', include(('authentication.urls', 'users'), namespace='users'))
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # if(settings.DEBUG):
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
